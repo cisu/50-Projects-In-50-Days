2e2baedc1051a1c37300528374dd93201253a7c7
cisu <49948070+cisu@users.noreply.github.com>
2021-02-08 21:28:17 +0200

    finish project

---
+++ day_20-Button_Ripple_Effect/main.js
@@ -0,0 +1,17 @@
+var buttons = document.querySelectorAll('.ripple');
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        var buttonTop = e.target.offsetTop;
+        var buttonLeft = e.target.offsetLeft;
+        var xInside = x - buttonLeft;
+        var yInside = y - buttonTop;
+        var circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left = xInside + 'px';
+        this.appendChild(circle);
+        setTimeout(function () { return circle.remove(); }, 500);
+    });
+});
