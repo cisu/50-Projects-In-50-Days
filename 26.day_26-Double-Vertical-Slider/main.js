50065946755f0224059e43a860f4d264528e0037
cisu <49948070+cisu@users.noreply.github.com>
2021-03-08 23:45:29 +0200

    finish js

--- 26.day_26-Double-Vertical-Slider/main.js
+++ 26.day_26-Double-Vertical-Slider/main.js
@@ -1,8 +1,8 @@
 var sliderContainer = document.querySelector('.slider-container');
 var slideRight = document.querySelector('.right-slide');
-var slideLeft = document.querySelector('.right-slide');
-var upButton = document.querySelector('.up-bottom');
-var downButton = document.querySelector('.down-bottom');
+var slideLeft = document.querySelector('.left-slide');
+var upButton = document.querySelector('.up-button');
+var downButton = document.querySelector('.down-button');
 var slidesLength = slideRight.querySelectorAll('div').length;
 var activeSlideIndex = 0;
 slideLeft.style.top = "-" + (slidesLength - 1) * 100 + "vh";
@@ -10,5 +10,18 @@ upButton.addEventListener('click', function () { return changeSlide('up'); });
 downButton.addEventListener('click', function () { return changeSlide('down'); });
 var changeSlide = function (direction) {
     var sliderHeight = sliderContainer.clientHeight;
-    console.log(sliderHeight);
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0;
+        }
+    }
+    else if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1;
+        }
+    }
+    slideRight.style.transform = "translateY(-" + activeSlideIndex * sliderHeight + "px)";
+    slideLeft.style.transform = "translateY(" + activeSlideIndex * sliderHeight + "px)";
 };
--- 26.day_26-Double-Vertical-Slider/main.ts
+++ 26.day_26-Double-Vertical-Slider/main.ts
@@ -1,8 +1,8 @@
 const sliderContainer = document.querySelector('.slider-container');
 const slideRight = document.querySelector('.right-slide');
-const slideLeft = document.querySelector('.right-slide');
-const upButton = document.querySelector('.up-bottom');
-const downButton = document.querySelector('.down-bottom');
+const slideLeft = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
 const slidesLength = slideRight.querySelectorAll('div').length;
 
 let activeSlideIndex = 0;
@@ -14,5 +14,23 @@ downButton.addEventListener('click', () => changeSlide('down'));
 
 const changeSlide = direction => {
   const sliderHeight = sliderContainer.clientHeight;
-  console.log(sliderHeight);
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
 };
